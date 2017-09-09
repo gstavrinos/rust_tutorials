75c50e62d3e1bd3f72fb1f5e7bd4aab2bc735237
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-09-09 15:39:04 +0300

    Fixed structure

---
+++ chapter3/functions/target/debug/functions.d
@@ -0,0 +1,1 @@
+/home/gstavrinos/rust_tutorials/functions/target/debug/functions: /home/gstavrinos/rust_tutorials/functions/src/main.rs
---
+++ chapter3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/gstavrinos/rust_tutorials/variables/target/debug/variables: /home/gstavrinos/rust_tutorials/variables/src/main.rs
