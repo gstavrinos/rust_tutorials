d2b65f5f0cf5bc91bc847d82f2f7cbc8291c18a0
George Stavrinos <gstavrinos@iit.demokritos.gr>
2017-09-19 21:18:57 +0300

    Generics done, time for traits!

---
+++ chapter10/largest_example/target/debug/deps/largest_example-ca541ad789c84842.d
@@ -0,0 +1,5 @@
+/home/gstavrinos/rust_tutorials/chapter10/largest_example/target/debug/deps/largest_example-ca541ad789c84842: src/main.rs
+
+/home/gstavrinos/rust_tutorials/chapter10/largest_example/target/debug/deps/largest_example-ca541ad789c84842.d: src/main.rs
+
+src/main.rs:
